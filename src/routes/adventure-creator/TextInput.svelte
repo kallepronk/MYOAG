fc30e1b2378235667eebc14deab0ba8a748868a3
Kalle Pronk <kallepronk@gmail.com>
2023-10-10 09:03:35 +0200

    add generic textInput component

---
+++ src/routes/adventure-creator/TextInput.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let description = "this is not the description you are looking for!";
+    export let input = "";
+</script>
+
+<label class="block w-full mb-4"> {description}
+    <textarea class="p-1 border-black block border-2 rounded-lg w-full" bind:value={input} required></textarea>
+</label>
