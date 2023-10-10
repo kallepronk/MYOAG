b78f35d9a9b334bf428d172075ff7fd10979394a
Kalle Pronk <kallepronk@gmail.com>
2023-10-10 09:02:22 +0200

    decomplicate the main page div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
     import "../app.css";
 </script>
 
-<div class="grid grid-cols-4 bg-amber-950 h-screen">
+<div class="grid grid-cols-4 bg-amber-950 h-full min-h-screen">
     <div class="col-span-1">
     </div>
-    <div class="bg-white col-span-2 p-5">
+    <div class="bg-white col-span-2 p-5 h-full w-full">
         <slot />
     </div>
     <div class="col-span-1">
