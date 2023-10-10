8a6a1e11e12edc9156fb91fdc3f580f8daa07c46
Kalle Pronk <kallepronk@gmail.com>
2023-10-10 09:03:20 +0200

    add generic modal component

---
+++ src/shared/Modal.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let showModal = false;
+</script>
+
+{#if showModal}
+    <div class="w-full h-full backdrop-blur bg-opacity-50 bg-black fixed top-0 left-0 grid content-center" on:click|self>
+        <div class="p-4 rounded-2xl max-w-xl bg-white text-center m-auto">
+            <slot name="title"></slot>
+            <slot></slot>
+        </div>
+    </div>
+{/if}
